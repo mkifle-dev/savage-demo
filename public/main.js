08ea2c189a44fe52ac4ac79f09f257550952e7b1
Miri <miriamkifle.dev@gmail.com>
2021-10-16 15:17:32 -0400

    update

--- public/main.js
+++ public/main.js
@@ -1,45 +1,79 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+const thumbUp = document.getElementsByClassName("fa-thumbs-up");
+const thumbsDown = document.getElementsByClassName("fa-thumbs-down");
+const trash = document.getElementsByClassName("fa-trash");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
+Array.from(thumbUp).forEach(function (element) {
+  // build an array from paramter
+
+  element.addEventListener("click", function () {
+    const name = this.parentNode.parentNode.childNodes[1].innerText;
+    const msg = this.parentNode.parentNode.childNodes[3].innerText;
+    const thumbUp = parseFloat(
+      this.parentNode.parentNode.childNodes[5].innerText
+    );
+    fetch("messages", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: name,
+        msg: msg,
+        thumbUp: thumbUp,
+       operation: '+', 
+      }),
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        window.location.reload(true);
       });
+  });
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
+Array.from(thumbsDown).forEach(function (element) {
+  element.addEventListener("click", function () {
+    const name = this.parentNode.parentNode.childNodes[1].innerText;
+    const msg = this.parentNode.parentNode.childNodes[3].innerText;
+    const thumbUpNum = parseFloat( // parsing string and float the interger...round up. 
+      this.parentNode.parentNode.childNodes[5].innerText
+    );
+
+    fetch("messages", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: name,
+        msg: msg,
+        thumbUp: thumbUpNum,
+        operation: '-'
+      }),
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        window.location.reload(true);
       });
+  });
+});
+
+Array.from(trash).forEach(function (element) {
+  element.addEventListener("click", function () {
+    const name = this.parentNode.parentNode.childNodes[1].innerText;
+    const msg = this.parentNode.parentNode.childNodes[3].innerText;
+    fetch("messages", {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        msg: msg,
+      }),
+    }).then(function (response) {
+      window.location.reload();
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express') 
+// express =framework that allow us to build out our API's...by create we mean the create (post), read (Get), update(Put) and Delete (Delete) requests
+const app = express() 
+//^^
+const bodyParser = require('body-parser') 
+// enables us to look @ request body; body parser enables us to look at that information. Now build into express. 
+const MongoClient = require('mongodb').MongoClient 
+// a way to connect to our MongoDb database. 
 
-var db, collection;
+var db, collection; 
+ // database * collection; var used for gloabl scoping. 
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true"; //conection to database 
+const dbName = "demo"; // name of database. 
 
-app.listen(3000, () => {
+app.listen(3000, () => {  
+    //this section is setting up server, and telling to connect to 3000
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
@@ -19,14 +25,21 @@ app.listen(3000, () => {
 });
 
 app.set('view engine', 'ejs')
+  // ejs looks just like html, but allows us plug content into html, without hardcoding. 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
+// reads the static files 
+// express handles all routing as long as in public folder. 
 
-app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+app.get('/', (req, res) => {  //both objects
+  db.collection('messages').find().toArray((err, result) => { //whereever we see result, that is the variable holding our array that has all of the documents from the database in it. 
+    // all docs were put into array that has the name of result. 
     if (err) return console.log(err)
     res.render('index.ejs', {messages: result})
+    // res= response ; req= request
+    //render out an html response...our response is going to be result of render method
+    // messages  is holding our array of data/objects. ()
   })
 })
 
@@ -39,10 +52,12 @@ app.post('/messages', (req, res) => {
 })
 
 app.put('/messages', (req, res) => {
+ req.body.operation === '-' ? req.body.thumbUp-- : req.body.thumbUp++
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
-      thumbUp:req.body.thumbUp + 1
+      
+      thumbUp: req.body.thumbUp
     }
   }, {
     sort: {_id: -1},
@@ -59,3 +74,4 @@ app.delete('/messages', (req, res) => {
     res.send('Message deleted!')
   })
 })
+
